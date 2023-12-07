fe015d95382070e91e5e4fa4cf4acaea67237c93
Will Squibb <wsquibb@andrew.cmu.edu>
2023-12-07 01:26:57 -0500

    Day 1 Solutions

--- d1/trebuchet.py
+++ d1/trebuchet.py
@@ -1,20 +1,35 @@
 # calibration = open("data/example1.txt", "r")
 # calibration = open("data/example2.txt", "r")
-calibration = open("data/input.txt", "r")
+calibration = open("data/trebuchet.txt", "r")
 
 lines = calibration.readlines()
 
-nums = ["one", "two", "three", "four", "five", "six", "seven", "eight", "nine"]
-
+# ======= Part 1 =======
 sum = 0
 for line in lines:
-  idx = 0
-  curr = ""
   
   size = 0
   digits = ""
   for char in line:
-    idx += 1
+    
+    if char.isdigit():
+      digits += char
+      size+=1
+          
+  code = int(digits[0]) * 10 + int(digits[size-1])
+  sum += code
+  
+print(sum)
+
+# ======= Part 2 =======
+nums = ["one", "two", "three", "four", "five", "six", "seven", "eight", "nine"]
+sum = 0
+for line in lines:
+  size = 0
+  digits = ""
+
+  curr = ""
+  for char in line:
     curr += char
     
     if char.isdigit():
--- d1/weather_machine.py
+++ d1/weather_machine.py
@@ -2,6 +2,17 @@ input = open("data/weather_machine.txt", "r")
 
 instructions = input.read()
 
+# ======= Part 1 =======
+level = 0
+for i in instructions:
+    if (i == '('):
+      level += 1
+    elif (i == ')'):
+      level -= 1
+    
+print(level)
+
+# ======= Part 2 =======
 level = 0
 index = 0
 for i in instructions:
